6d420f6a9196a690826662fc95023cbffed32dee
tmsbkn <54986052+tmsbkn@users.noreply.github.com>
2026-04-19 11:05:45 -0400

    Convert choice to string

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3);
+    let choice = Math.floor(Math.random() * 3);
+    if (choice == 0) {
+        choice = "Rock";
+    } else if (choice == 1) {
+        choice = "Paper";
+    } else {
+        choice = "Scissors";
+    }
+    
+    return choice;
 };
 
 console.log(getComputerChoice());
