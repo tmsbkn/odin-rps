73db44c1fd095756c061ce212194785ae5bc75d5
tmsbkn <54986052+tmsbkn@users.noreply.github.com>
2026-04-19 13:25:15 -0400

    Add multiple rounds to playGame function

--- script.js
+++ script.js
@@ -17,36 +17,48 @@ function getUserChoice() {
     return choice;
 };
 
-let computerScore = 0;
-let humanScore = 0;
 
-function playRound(humanChoice,computerChoice){
-    let roundOutcome;
-    if (humanChoice == computerChoice) {
-        roundOutcome = 'Tie, play again';
-    } else if (
+
+function playGame(){
+    let computerScore = 0;
+    let humanScore = 0;
+
+    function playRound(humanChoice,computerChoice){
+        let roundOutcome;
+        if (humanChoice == computerChoice.toUpperCase()) {
+          roundOutcome = 'Tie, play again';
+         ;
+        } else if (
         computerChoice == 'ROCK' && humanChoice.toUpperCase() == 'PAPER' ||
         computerChoice == 'PAPER' && humanChoice.toUpperCase() == 'SCISSORS' || 
         computerChoice == 'SCISSORS' && humanChoice.toUpperCase() == 'ROCK'
-    ){
-        roundOutcome = 'You Win! ' + humanChoice + ' beats ' + computerChoice;
-        humanScore = humanScore++;
-    } else if (
+        ){
+            roundOutcome = 'You Win! ' + humanChoice + ' beats ' + computerChoice;
+            humanScore++;
+        } else if (
         computerChoice == 'ROCK' && humanChoice.toUpperCase() == 'SCISSORS' ||
         computerChoice == 'PAPER' && humanChoice.toUpperCase() == 'ROCK' ||
         computerChoice == 'SCISSORS' && humanChoice.toUpperCase() == 'PAPER'
         ){
-        roundOutcome = 'You Lose! ' + computerChoice + ' beats ' + humanChoice;
-        computerScore = computerScore++;
-    } else {
+            roundOutcome = 'You Lose! ' + computerChoice + ' beats ' + humanChoice;
+            computerScore++;
+        } else {
         roundOutcome = 'Type either ROCK, PAPER, or SCISSORS';
+        }
+        console.log(roundOutcome);
     }
 
-    console.log(roundOutcome);
-    
+    playRound(getUserChoice(), getComputerChoice());
+    console.log("Human score is ", humanScore, " Computer score is ",computerScore);
+    playRound(getUserChoice(), getComputerChoice());
+    console.log("Human score is ", humanScore, " Computer score is ",computerScore);
+    playRound(getUserChoice(), getComputerChoice());
+    console.log("Human score is ", humanScore, " Computer score is ",computerScore);
+    playRound(getUserChoice(), getComputerChoice());
+    console.log("Human score is ", humanScore, " Computer score is ",computerScore);
+    playRound(getUserChoice(), getComputerChoice());
+    console.log("Human score is ", humanScore, " Computer score is ",computerScore);
     
 }
-    const humanSelection = getUserChoice();
-    const computerSelection = getComputerChoice();
 
-    playRound(humanSelection, computerSelection);
+    playGame();
